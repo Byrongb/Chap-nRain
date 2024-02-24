fd7202a9d216fab2db61be3ad890ae9198d220e7
Byron <bgguerra@scl.edu.gt>
2024-02-24 17:25:28 -0600

    diseño de los diferentes menus

--- js/js2/jsMenús/bodyMainC.js
+++ js/js2/jsMenús/bodyMainC.js
@@ -19,17 +19,164 @@ document.addEventListener('DOMContentLoaded', function() {
             </ul>
         </div>
         <div class="contentMenus">
-            <div class="content-box">Contenido 1</div>
-            <img src="ruta_a_tu_imagen.jpg" alt="Imagen 1" class="content-image">
-            <div class="content-box">Contenido 2</div>
-            <img src="ruta_a_tu_imagen.jpg" alt="Imagen 2" class="content-image">
+
+        <div class="content-box">
+
+        <h2 class="menu-titleM">MAIN COURSES</h2>
+
+            <div class="menu-itemsM">
+                <div class="menu-itemM">
+                <div class="menu-item-image">
+                    <img src="ruta_a_la_imagen_del_platillo.jpg" alt="Imagen del Platillo">
+                </div>
+                    <h3>Platillo 1</h3>
+                    <p>Descripción del platillo 1. Un breve resumen de lo que incluye.</p>
+                    <span class="price">$10.00</span>
+                </div>
+
+                <div class="menu-itemM">
+                <div class="menu-item-image">
+                    <img src="ruta_a_la_imagen_del_platillo.jpg" alt="Imagen del Platillo">
+                </div>
+                    <h3>Platillo 1</h3>
+                    <p>Descripción del platillo 1. Un breve resumen de lo que incluye.</p>
+                    <span class="price">$10.00</span>
+                </div>
+
+                <div class="menu-itemM">
+                <div class="menu-item-image">
+                    <img src="ruta_a_la_imagen_del_platillo.jpg" alt="Imagen del Platillo">
+                </div>
+                    <h3>Platillo 1</h3>
+                    <p>Descripción del platillo 1. Un breve resumen de lo que incluye.</p>
+                    <span class="price">$10.00</span>
+                </div>
+
+                <div class="menu-itemM">
+                <div class="menu-item-image">
+                    <img src="ruta_a_la_imagen_del_platillo.jpg" alt="Imagen del Platillo">
+                </div>
+                    <h3>Platillo 1</h3>
+                    <p>Descripción del platillo 1. Un breve resumen de lo que incluye.</p>
+                    <span class="price">$10.00</span>
+                </div>
+
+                <div class="menu-itemM">
+                <div class="menu-item-image">
+                    <img src="ruta_a_la_imagen_del_platillo.jpg" alt="Imagen del Platillo">
+                </div>
+                    <h3>Platillo 1</h3>
+                    <p>Descripción del platillo 1. Un breve resumen de lo que incluye.</p>
+                    <span class="price">$10.00</span>
+                </div>
+
+                <div class="menu-itemM">
+                <div class="menu-item-image">
+                    <img src="ruta_a_la_imagen_del_platillo.jpg" alt="Imagen del Platillo">
+                </div>
+                    <h3>Platillo 1</h3>
+                    <p>Descripción del platillo 1. Un breve resumen de lo que incluye.</p>
+                    <span class="price">$10.00</span>
+                </div>
+            </div>
+        </div>
+
+            <img src="https://scontent-sea1-1.xx.fbcdn.net/v/t39.30808-6/429492497_1146767052981618_3904851544229404517_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=3635dc&_nc_ohc=Ne4bEIyx0hAAX-QcA1e&_nc_ht=scontent-sea1-1.xx&oh=00_AfCY8Rr88GYOmyXQDtEDFDEQQq3U2k-1xZS8E2cHrTfpOQ&oe=65DEC780" alt="Imagen 1" class="content-image">            
+
+            <div class="content-box">
+            <h2 class="menu-titleM">APPETIZERS</h2>
+            <div class="menu-itemsM">
+
+                <div class="menu-itemM">
+                <div class="menu-item-image">
+                    <img src="ruta_a_la_imagen_del_platillo.jpg" alt="Imagen del Platillo">
+                </div>
+                    <h3>Platillo 1</h3>
+                    <p>Descripción del platillo 1. Un breve resumen de lo que incluye.</p>
+                    <span class="price">$10.00</span>
+                </div>
+
+                <div class="menu-itemM">
+                <div class="menu-item-image">
+                    <img src="ruta_a_la_imagen_del_platillo.jpg" alt="Imagen del Platillo">
+                </div>
+                    <h3>Platillo 1</h3>
+                    <p>Descripción del platillo 1. Un breve resumen de lo que incluye.</p>
+                    <span class="price">$10.00</span>
+                </div>
+
+                <div class="menu-itemM">
+                <div class="menu-item-image">
+                    <img src="ruta_a_la_imagen_del_platillo.jpg" alt="Imagen del Platillo">
+                </div>
+                    <h3>Platillo 1</h3>
+                    <p>Descripción del platillo 1. Un breve resumen de lo que incluye.</p>
+                    <span class="price">$10.00</span>
+                </div>
+
+                <div class="menu-itemM">
+                <div class="menu-item-image">
+                    <img src="ruta_a_la_imagen_del_platillo.jpg" alt="Imagen del Platillo">
+                </div>
+                    <h3>Platillo 1</h3>
+                    <p>Descripción del platillo 1. Un breve resumen de lo que incluye.</p>
+                    <span class="price">$10.00</span>
+                </div>
+
+                <div class="menu-itemM">
+                <div class="menu-item-image">
+                    <img src="ruta_a_la_imagen_del_platillo.jpg" alt="Imagen del Platillo">
+                </div>
+                    <h3>Platillo 1</h3>
+                    <p>Descripción del platillo 1. Un breve resumen de lo que incluye.</p>
+                    <span class="price">$10.00</span>
+                </div>
+
+                <div class="menu-itemM">
+                <div class="menu-item-image">
+                    <img src="ruta_a_la_imagen_del_platillo.jpg" alt="Imagen del Platillo">
+                </div>
+                    <h3>Platillo 1</h3>
+                    <p>Descripción del platillo 1. Un breve resumen de lo que incluye.</p>
+                    <span class="price">$10.00</span>
+                </div>
+
+            </div>
+        </div>
+
+            <img src="https://scontent-sea1-1.xx.fbcdn.net/v/t39.30808-6/429582833_1146767049648285_6633792576356285569_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=3635dc&_nc_ohc=-X8bm2h03HMAX8fsssW&_nc_ht=scontent-sea1-1.xx&oh=00_AfB-PzMX2rC-Nd7PAO-Dj8O3Ix5NlFGD8GwenKQ_j7sxpg&oe=65DED38E" alt="Imagen 2" class="content-image">
         </div>
     </div>
+    <div id="footer-container"></div>
+
     `;
 
     // Asumiendo que tienes un elemento en tu HTML donde quieres insertar esta sección.
     // Por ejemplo justo después del header. Asegúrate de tener un contenedor con id="main-content" o ajusta este selector según tu estructura.
     document.body.insertBefore(heroSection, document.body.firstChild.nextSibling);
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    // Selecciona todos los ítems del menú
+    const menuItems = document.querySelectorAll('.menu-itemM');
+
+    // Función para mostrar la imagen
+    function showImage(e) {
+        const imageContainer = e.currentTarget.querySelector('.menu-item-image');
+        imageContainer.classList.add('menu-item-image-visible');
+    }
+
+    // Función para ocultar la imagen
+    function hideImage(e) {
+        const imageContainer = e.currentTarget.querySelector('.menu-item-image');
+        imageContainer.classList.remove('menu-item-image-visible');
+    }
+
+    // Agrega event listeners a cada ítem del menú
+    menuItems.forEach(item => {
+        item.addEventListener('mouseenter', showImage);
+        item.addEventListener('mouseleave', hideImage);
+    });
 });
 
 document.addEventListener('DOMContentLoaded', function() {
@@ -49,3 +196,47 @@ document.addEventListener('DOMContentLoaded', function() {
 
     requestAnimationFrame(parallax); // Inicia la animación
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+    const footer = document.getElementById('footer-container');
+        footer.innerHTML =  `
+        <div class="footer">
+        <div class="footer-column">
+        <div class="logoF">Chapín<br><span>Rain</span></div>
+          <p>Committed to excellence.</p>
+          <div class="social-icons">
+            <img src="icon-facebook.png">
+            <img src="icon-twitter.png">
+            <img src="icon-instagram.png">
+          </div>
+        </div>
+        <div class="footer-column">
+          <h3>DESCRIPCION GENERAL</h3>
+          <a id="home" href="#" class="general-link">Home</a>
+          <a id="menu" href="#" class="general-link">Menus</a>
+          <a id="deliv" href="#"  class="general-link">Online delivery</a>
+          <a id="about" href="#" class="general-link">About us</a>
+        </div>
+        <div class="footer-column">
+          <h3>RECURSOS</h3>
+          <a href="#" class="resource-link">Help Center</a>
+          <a href="#" class="resource-link">Terms of use</a>
+          <a href="#" class="resource-link">Privacy Policy</a>
+        </div>
+        <div class="footer-column">
+          <h3 class="footerFont">CONTACT INFORMATION</h3>
+          <p><img src="icon-phone.png">(206) 123-4567</p>
+          <p><img src="icon-email.png">email@example.com</p>
+          <p><img src="icon-url.png">www.example.com</p>
+        </div>
+        <div class="footer-column">
+          <h3>LOCATION</h3>
+          <span class="address-icon"><i class="fas fa-map-marker-alt"></i> 1234 Pike St, Seattle, WA 98101</span>
+        </div>
+      </div>
+      `;
+      document.getElementById('home').addEventListener('click', function() { window.location.href = 'index.html'; });
+      document.getElementById('about').addEventListener('click', function() { window.location.href = 'about.html'; });
+      document.getElementById('deliv').addEventListener('click', function() { window.location.href = 'delivery.html'; });
+      
+    });
